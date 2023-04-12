63cd5af174462822ba0fcfe2102f730b1524d5fa
Lintangms14 <merayusukmalintang@gmail.com>
2023-04-12 14:34:18 +0700

    void input data

--- Latihan_OOP/Latihan_OOP.cpp
+++ Latihan_OOP/Latihan_OOP.cpp
@@ -5,4 +5,11 @@ class Mahasiswa {
 private:
 	int nim;
 	string nama;
+public:
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan Nama : ";
+		cin >> nama;
+	}
 };
